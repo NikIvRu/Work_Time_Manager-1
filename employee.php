b1be03d6b873f7314a8e9faabd66a4ff287ccf01
kokirusev@abv.bg <NikIvRu@NikIvRu-PC.(none)>
2012-03-07 17:04:20 +0200

    4rd c ephp

--- employee.php
+++ employee.php
@@ -4,5 +4,7 @@ $e_id=$_POST["ID"];
 $e_password=$_POST["Password"];
 $e_fname=$_POST["First Name"];
 $e_lname=$_POST["Last name"];
+$e_position=$_POST["Positiion"];
+$last_activity=$_POST["Last Activity"];
 
 ?>
