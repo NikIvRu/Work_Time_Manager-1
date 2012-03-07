c3cebda22b2ee2aafa99601def5270433d9cf575
kokirusev@abv.bg <NikIvRu@NikIvRu-PC.(none)>
2012-03-07 17:05:29 +0200

    5 c ephp

--- employee.php
+++ employee.php
@@ -7,4 +7,9 @@ $e_lname=$_POST["Last name"];
 $e_position=$_POST["Positiion"];
 $last_activity=$_POST["Last Activity"];
 
+if($e_id!="" && $e_password!="" && $e_fname!="" && $e_lname!="" && $position!=""
+	&& $last_activity!=""){
+	
+	}
+
 ?>
